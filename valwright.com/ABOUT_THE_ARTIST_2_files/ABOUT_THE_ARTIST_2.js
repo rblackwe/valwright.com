9b18f025dec64376e3e299e604f8c7d72b553fb0
Val Wright <val@valwright.com>
2011-01-18 14:57:42 -0500

    auto commit from publish script

---
+++ valwright.com/ABOUT_THE_ARTIST_2_files/ABOUT_THE_ARTIST_2.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.2 local-build-20110118
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,106),url:'ABOUT_THE_ARTIST_2_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'ABOUT_THE_ARTIST_2_files/stroke_1.png'},{rect:new IWRect(2,-2,106,4),url:'ABOUT_THE_ARTIST_2_files/stroke_2.png'},{rect:new IWRect(108,-2,4,4),url:'ABOUT_THE_ARTIST_2_files/stroke_3.png'},{rect:new IWRect(108,2,4,106),url:'ABOUT_THE_ARTIST_2_files/stroke_4.png'},{rect:new IWRect(108,108,4,4),url:'ABOUT_THE_ARTIST_2_files/stroke_5.png'},{rect:new IWRect(2,108,106,4),url:'ABOUT_THE_ARTIST_2_files/stroke_6.png'},{rect:new IWRect(-2,108,4,4),url:'ABOUT_THE_ARTIST_2_files/stroke_7.png'}],new IWSize(110,110))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('ABOUT_THE_ARTIST_2_files/ABOUT_THE_ARTIST_2Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
